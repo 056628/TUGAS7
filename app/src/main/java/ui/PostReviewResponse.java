decb9bfe2790ca868b8fc78b29886069fd31d938
McTavish <12050116173@students.uin-suska.ac.id>
2024-04-21 21:21:27 +0700

    Initial commit

---
+++ app/src/main/java/ui/PostReviewResponse.java
@@ -0,0 +1,30 @@
+package ui;
+
+import com.google.gson.annotations.SerializedName;
+        import java.util.List;
+
+import response.CustomerReviewsItem;
+
+public class PostReviewResponse {
+
+    @SerializedName("customerReviews")
+    private List<CustomerReviewsItem> customerReviews;
+
+    @SerializedName("error")
+    private boolean error;
+
+    @SerializedName("message")
+    private String message;
+
+    public List<CustomerReviewsItem> getCustomerReviews() {
+        return customerReviews;
+    }
+
+    public boolean isError() {
+        return error;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
